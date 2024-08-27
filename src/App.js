cfc3aa6485d765727ca76b039f184310af5e384c
jubairskv <jubairskv@gmail.com>
2024-08-28 00:51:49 +0530

    Oauth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { GoogleLogin } from "@react-oauth/google";
 import "./App.css";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
   return (
     <div className="App">
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
+          const decoded = jwtDecode(credentialResponse?.credential);
+          console.log(decoded);
         }}
         onError={() => {
           console.log("Login Failed");
